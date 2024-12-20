8f9f1258a68b28d2b23246f50af21849c8c00804
Will Fox <william.macdonald.fox@gmail.com>
2024-12-20 17:22:45 -0500

    test: write tests for hit and sunk? methods

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -17,9 +17,40 @@ RSpec.describe Ship do
         it 'has a length' do
             expect(@cruiser.length).to eq(3)
         end
-        
+
         it 'has a health level' do
             expect(@cruiser.health).to eq(3)
         end
     end
+
+    describe '#hit' do 
+        it 'records a hit on a ship and decreases health by one' do 
+            expect(@cruiser.health).to eq(3)
+            expect(@cruiser.sunk?).to eq(false)
+
+            @cruiser.hit
+            expect(@cruiser.health).to eq(2)
+        end
+    end
+
+    describe '#sunk?' do 
+        it 'asks if a ship is sunk and defaults to false' do 
+            expect(@cruiser.health).to eq(3)
+            expect(@cruiser.sunk?).to eq(false)
+        end
+
+        it 'returns true if health is zero' do 
+            expect(@cruiser.health).to eq(3)
+            expect(@cruiser.sunk?).to eq(false)
+
+            @cruiser.hit
+            expect(@cruiser.health).to eq(2)
+
+            @cruiser.hit
+            expect(@cruiser.health).to eq(1)
+
+            @cruiser.hit
+            expect(@cruiser.sunk?).to eq(true)
+        end
+    end 
 end
