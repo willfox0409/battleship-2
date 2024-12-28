a07610e5e2bebe7e8ac69991ae7b1b4cbfc86929
Will Fox <william.macdonald.fox@gmail.com>
2024-12-28 13:10:58 -0500

    test: add test for board initialization with 16 cell objects in a hash

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe Board do
         it 'exists' do 
             expect(@board).to be_a(Board) 
         end
+
+        it 'holds 16 cell objects in a hash' do 
+            expect(@board.cells).to be_a(Hash)
+            expect(@board.cells.count).to eq(16)
+            expect(@board.cells["A1"]).to be_a(Cell)
+            expect(@board.cells["B2"]).to be_a(Cell)
+            expect(@board.cells["C3"]).to be_a(Cell)
+            expect(@board.cells["D4"]).to be_a(Cell)
+        end
     end
 end
