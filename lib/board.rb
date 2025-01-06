81278ed7aa41031dc95b45f5032558d75a10b6c1
Coral <coralruschak@gmail.com>
2025-01-06 17:36:52 -0500

    Feat: Render Method for Board Class

--- lib/board.rb
+++ lib/board.rb
@@ -27,8 +27,8 @@ class Board
     end
 
     def place(ship, coordinates)
-        coordinates.each do |coordinate|
-            @cells[coordinate].place_ship(ship)
+        coordinates.each do |coordinate| 
+             @cells[coordinate].place_ship(ship)
         end
     end
 
@@ -70,5 +70,21 @@ class Board
         empty_cells?(coordinates) &&
         valid_length?(ship, coordinates) &&
         (horizontal?(coordinates) || vertical?(coordinates))
+    end
+
+    def render(board_show_ships = false)
+      if board_show_ships == true
+        "  1 2 3 4 \n" +
+        "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
+        "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
+        "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
+        "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
+      else
+        "  1 2 3 4 \n" +
+        "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
+        "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
+        "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
+        "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+      end
     end
 end
