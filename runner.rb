c2ac36944f5d47ec54cd5cbae000b0ff654d6b34
Will Fox <william.macdonald.fox@gmail.com>
2025-01-08 16:09:32 -0500

    fix: update indentation for case/when statements for readability

--- runner.rb
+++ runner.rb
@@ -11,25 +11,25 @@ player_input = gets.chomp.downcase
 
 case player_input
 when "p"
-  game = Game.new
-  game.computer_cruiser_setup
-  game.computer_submarine_setup
-  game.player_cruiser_setup
-  game.player_submarine_setup
+    game = Game.new
+    game.computer_cruiser_setup
+    game.computer_submarine_setup
+    game.player_cruiser_setup
+    game.player_submarine_setup
 
-  game.display_boards
+    game.display_boards
 
-  while game.game_over == false
-    game.player_turn
-    game.computer_turn
-  end
+        while game.game_over == false
+            game.player_turn
+            game.computer_turn
+        end
 when "q"
-  puts ""
-  puts "Please return again when you wish to play."
-  puts ""
-  puts "Goodbye for now!"
-  puts ""
-  exit
+    puts ""
+    puts "Please return again when you wish to play."
+    puts ""
+    puts "Goodbye for now!"
+    puts ""
+    exit
 else
-  puts "Incorrect entry, please restart the game."
+    puts "Incorrect entry, please restart the game."
 end
