a439f7ed0aed3ea86471c0bb192f1aca068a7d71
Will Fox <william.macdonald.fox@gmail.com>
2025-01-08 16:02:39 -0500

    fix: move the #main_menu fucntion to the runner file instead of the game class

--- runner.rb
+++ runner.rb
@@ -3,19 +3,33 @@ require './lib/cell'
 require './lib/board'
 require './lib/game'
 
-puts "Welcome to BATTLESHIP \n" + "Enter p to play. Enter q to quit."
-player_input = gets.chomp
-if player_input == "p"
-    game = Game.new
-    game.computer_cruiser_setup
-    game.computer_submarine_setup
-    game.player_cruiser_setup
-    game.player_submarine_setup
+puts "Welcome to BATTLESHIP \n"
+puts ""
+puts "Enter p to play. Enter q to quit. \n>"
 
-    game.display_boards
-    
-    while game.game_over == false
-        game.player_turn
-        game.computer_turn
-    end
+player_input = gets.chomp.downcase
+
+case player_input
+when "p"
+  game = Game.new
+  game.computer_cruiser_setup
+  game.computer_submarine_setup
+  game.player_cruiser_setup
+  game.player_submarine_setup
+
+  game.display_boards
+
+  while game.game_over == false
+    game.player_turn
+    game.computer_turn
+  end
+when "q"
+  puts ""
+  puts "Please return again when you wish to play."
+  puts ""
+  puts "Goodbye for now!"
+  puts ""
+  exit
+else
+  puts "Incorrect entry, please restart the game."
 end
